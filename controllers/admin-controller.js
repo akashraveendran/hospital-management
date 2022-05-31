943931b39f2fc07c4a024534338762e39832820b
Akash Raveendran <akashraveendran1611@gmail.com>
2022-05-31 16:18:30 +0530

    initialised all the controller functions for admin route

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,9 +3,75 @@
 const getHomePage = (req, res) => {
     res.render('index', { title: 'Admin' });
 }
+const getLoginPage = (req, res) => {
+    res.send("Login page get request");
+}
+const doLogin = (req, res) => {
+    res.send("Login page post request");
+}
+const getAllHopitals = (req, res) => {
+    res.send("all hospitals request");
+}
+const deleteHospital = (req, res) => {
+    res.send("delete hospital request");
+}
+const addHospitalPage = (req, res) => {
+    res.send("add hospital page get request");
+}
+const createHospital = (req, res) => {
+    res.send("add hospital page post request");
+}
+const getAllClinics = (req, res) => {
+    res.send("all hospitals request");
+}
+const addClinicPage = (req, res) => {
+    res.send("add clinic page get request");
+}
+const createClinic = (req, res) => {
+    res.send("add clinic page post request");
+}
+const deleteClinic = (req, res) => {
+    res.send("delete clinic page  request");
+}
+const viewAllMessages = (req, res) => {
+    res.send("view clinic messages")
+}
+const sendMessagePage = (req, res) => {
+    res.send("send message get request")
+}
+const sendMessage = (req, res) => {
+    res.send("send message to admin")
+}
+
+const viewCheckupDatesPage = (req, res) => {
+    res.send("checkup dates")
+}
+const viewAllUsersPage = (req, res) => {
+    res.send("view all users")
+}
+const deleteUser = (req, res) => {
+    res.send("delete user")
+}
+
 
 
 
 module.exports = {
-    getHomePage
+    getHomePage,
+    getLoginPage,
+    doLogin,
+    getAllHopitals,
+    addHospitalPage,
+    createHospital,
+    deleteHospital,
+    getAllClinics,
+    addClinicPage,
+    createClinic,
+    deleteClinic,
+    viewAllMessages,
+    sendMessagePage,
+    sendMessage,
+    viewCheckupDatesPage,
+    viewAllUsersPage,
+    deleteUser
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,31 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-const { getHomePage } = require("../controllers/admin-controller")
+const {
+    getHomePage,
+    getLoginPage,
+    doLogin,
+    getAllHopitals,
+    addHospitalPage,
+    createHospital,
+    deleteHospital,
+    getAllClinics,
+    addClinicPage,
+    createClinic,
+    deleteClinic,
+    viewAllMessages,
+    sendMessagePage,
+    sendMessage,
+    viewCheckupDatesPage,
+    viewAllUsersPage,
+    deleteUser
+} = require("../controllers/admin-controller")
 
 /* GET home page. */
 router.get('/', getHomePage);
-router.get("/login")
-router.post("/login")
-router.get("/view-all-hospitals")
-router.get("/delete-hospital/:id")
+router.get("/login", getLoginPage)
+router.post("/login", doLogin)
+router.get("/view-all-hospitals", getAllHopitals)
+router.get("/delete-hospital/:id", deleteHospital)
 // router.get("/update-hospital/:id")
 // router.post("/update-hospital/:id")
-router.get("/add-hospital")
-router.post("/add-hospital")
-router.get("/delete-hospital/:id")
-router.get("/view-all-clinics")
-router.get("/add-clinic")
-router.post("/add-clinic")
-router.get("/delete-clinic/:id")
+router.get("/add-hospital", addHospitalPage)
+router.post("/add-hospital", createHospital)
+router.get("/view-all-clinics", getAllClinics)
+router.get("/add-clinic", addClinicPage)
+router.post("/add-clinic", createClinic)
+router.get("/delete-clinic/:id", deleteClinic)
 // router.get("/update-clinic/:id")
 // router.post("/update-clinic/:id")
-router.get("/view-messages")
-router.get("/send-message")
-router.post("/send-message")
-router.get("/view-checkup-dates")
-router.get("/view-all-users")
-router.get("/delete/user/:id")
+router.get("/view-messages", viewAllMessages)
+router.get("/send-message", sendMessagePage)
+router.post("/send-message", sendMessage)
+router.get("/view-checkup-dates", viewCheckupDatesPage)
+router.get("/view-all-users", viewAllUsersPage)
+router.get("/delete/user/:id", deleteUser)
 
 
 module.exports = router;
