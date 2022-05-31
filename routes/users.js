b251dee5a39b9c590c7e77c7c01287a32e382b40
Akash Raveendran <akashraveendran1611@gmail.com>
2022-05-31 15:55:27 +0530

    added all the requests in user route

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,20 @@ const { getHomePage } = require("../controllers/user-controller")
 
 /* GET home page. */
 router.get('/', getHomePage);
+router.get('/signup');
+router.post('/signup');
+router.get('/login');
+router.post('/login');
+router.get('/view-profile');
+router.get('/update-profile');
+router.post('/update-profile');
+router.get('/view-hospitals');
+router.get('/view-clinics');
+router.post('/search');//get the searched category from req.body
+router.get('/book-hospital/:id');
+router.get('/book-clinic/:id');
+router.get('/view-messages');
+router.get('/send-message');
+router.post('/send-message');
 
 module.exports = router;
